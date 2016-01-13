cc2449a97502db07e35d632c5a0cf59a8cd16814
emiliu <emilyliu10@gmail.com>
2016-01-12 22:19:46 -0500

    more aggressive i loop when shooting

--- flywheel.c
+++ flywheel.c
@@ -1,24 +1,10 @@
 /* no config code here. sorry. */
 
-/** hi there
-okay so basically add/change/fix/update/test stuff wherever there are TODO's
-line 31 -  you're going to have to test for the best value of gain
-	just by incrementing it slowly. higher gain means the power
-	changes faster, and the gain should not have to be greater than 1.
-line 39 - this setup may or may not be right
-line 48 - choose a motor with an encoder on it. also check the encoder box
-	in the configuration. it only uses one motor from one side, which
-	should work but if there are problems we can monitor a second motor.
-line 112 - self explanatory? you can use setVelocity() each time a
-	button is pressed.
-line 114 - you're going to have to test for the optimal velocities.
-	the first parameter into setVelocity() is the motor speed
-	(which is / should be printed in the debug stream) and the second
-	is the estimated power needed as a percent of full power.
-**/
-
 #define COUNTS_PER_REV 627.2
 #define TBH_LOOP_SPEED 30 /* change this as necessary */
+
+#define GAIN_NORMAL 0.001; /* TODO changing speeds */
+#define GAIN_HIGHER 0.035; /* TODO when shooting */
 
 long enc, enc_prev;
 
@@ -26,9 +12,8 @@ float motor_velocity;
 long time;
 
 long target_velocity;
-float current_error;
-float last_error;
-float gain = 0.00024; /* TODO this is the variable to fiddle around with */
+float current_error, last_error;
+float gain; /* integration coefficient constant */
 float drive, drive_prev; /* float between 0 and 1, indicating percent of full power */
 long first_cross;
 float drive_approx;
@@ -36,7 +21,7 @@ float drive_approx;
 long motor_drive;
 
 void setMotorPower(int power) {
-	/* TODO fix negatives as needed lol */
+	/* TODO fix negatives as needed */
 	// power is always positive
 	motor[lt] = power;
 	motor[rt] = power;
@@ -57,18 +42,18 @@ void setVelocity(int velocity, float predicted_drive) {
 	drive_approx = predicted_drive;
 	first_cross = 1;
 	drive_prev = 0;
+	gain = GAIN_NORMAL;
 }
 
 void calculateVelocity() {
 	/* update global variables */
-	int d_time;
-	int d_enc;
+	int d_time, d_enc;
 	enc = getEncoderCount();
 	d_time = nSysTime - time;
 	time = nSysTime;
 	d_enc = (enc - enc_prev);
 	enc_prev = enc;
-	motor_velocity = (1000.0 / d_ms) * d_enc * 60.0 / COUNTS_PER_REV;
+	motor_velocity = (1000.0 / d_time) * d_enc * 60.0 / COUNTS_PER_REV;
 	// print to debug stream
 	writeDebugStreamLine("motor velocity = %1.3f", motor_velocity);
 }
@@ -77,10 +62,7 @@ void updateVelocity() {
 
 	/* calculate and constrain drive */
 	current_error = target_velocity - motor_velocity;
-	if (abs(current_error * gain) < 0.3)
-		drive += current_error * gain;
-	else
-		drive += 0.3 * sgn(current_error * gain);
+	drive += current_error * gain;
 	if (drive > 1) drive = 1;
 	if (drive < 0) drive = 0;
 
@@ -89,6 +71,7 @@ void updateVelocity() {
 		if (first_cross) {
 			drive = drive_approx;
 			first_cross = 0;
+			if (target_velocity) gain = GAIN_HIGHER;
 		} else {
 			/* the tbh part ! */
 			drive = 0.5 * (drive + drive_prev);
